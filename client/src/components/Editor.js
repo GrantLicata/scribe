bcfb181ede4df586ee91f567a4bea1433dcdcc6c
GrantLicata <grant.licata@gmail.com>
2022-11-30 15:42:29 -0800

    Building the editor

---
+++ client/src/components/Editor.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Editor extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='editor-container'>
+                Hello World
+            </div>
+        )
+    }
+}
+
+export default Editor;
